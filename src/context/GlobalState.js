d2daa26ecb24ee20e3125683d76bb5668321509b
Ishaq Afridi <ishaqafridi@example.com>
2025-12-08 05:46:04 -0800

    new commit

---
+++ src/context/GlobalState.js
@@ -0,0 +1,29 @@
+import React,{createContext , useReducer} from "react";
+
+
+//initialState
+const initialState = {
+    transactions: [
+  { id: 1, text: 'Flower', amount: -20 },
+  { id: 2, text: 'Salary', amount: 300 },
+  { id: 3, text: 'Book', amount: -10 },
+  { id: 4, text: 'Camera', amount: 150 }
+]
+}
+ //createContext
+
+ export const GlobalContext = createContext(initialState);
+
+ //provider component
+
+ export const GlobalProvider = ({childeren}) => {
+    const [state , dispatch] = useReducer(AppReducer, initialState);
+ 
+ return (
+    <GlobalContext.Provider value ={{
+        transactions: state.transactions
+    }}>
+        {childeren}
+    </GlobalContext.Provider>
+ )
+ }
